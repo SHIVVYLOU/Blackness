67bd624cf01cf99bf6811559bc50ff9fc4bbb061
Ubuntu <ubuntu@ip-172-31-81-77.ec2.internal>
2019-07-29 15:38:13 +0000

    correcting error from app.py & setting environ variable

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import os
-from flask import Flask
-from flask_pymongo import Pymongo
-from bson.objectid import ObjectId
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
-app.config["MONGO_DBNAME"] = "task_manager"
-app.config["MONGO_URI"] = ""
+mongo = PyMongo(app)
 
-@app.route("/")
-def hello():
-    return "Hello World"
-    
-if __name__ == "__main__":
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
-        
+            port=int(os.environ.get('PORT')),
+            debug=True)
